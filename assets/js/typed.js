6035c438f89b3b191476850df596ae00bb8a4f79
khangpt2k6 <2006tuankhang@gmail.com>
2025-03-16 23:38:51 -0500

    Portfolio

---
+++ assets/js/typed.js
@@ -0,0 +1,7 @@
+var typed = new typed(".text", {
+  strings: ["Computer Science Student", "Full-Stack Developer", "Web Developer", "Software Developer"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true
+});
